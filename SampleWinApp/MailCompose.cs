94cae340f7cdb232c7fe39369f604f08a46c60a0
ramsm <rams.m@live.in>
2013-12-12 22:41:49 +0530

    Mail feature

--- SampleWinApp/MailCompose.cs
+++ SampleWinApp/MailCompose.cs
@@ -20,8 +20,20 @@ namespace SampleWinApp
             MailMessage mail = new MailMessage();
             mail.From = new MailAddress("");
 
-            SmtpClient mailClient = new SmtpClient();
-            mailClient.Send(mail);
+            //SmtpClient mailClient = new SmtpClient();
+            //mailClient.Credentials = "myusername@gmail.com";
+            //mailClient.EnableSsl = true;
+
+            //mailClient.Send(mail);
+
+
+            //var client = new SmtpClient("smtp.gmail.com", 587)
+            //{
+            //    Credentials = new NetworkCredential("myusername@gmail.com", "mypwd"),
+            //    EnableSsl = true
+            //};
+
+
         }
     }
 }
